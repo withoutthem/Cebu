a22dedb79b95e441ecf88e9b2355e9642334518a
Victor Lee <withoutthem50@gmail.com>
2025-08-10 18:18:59 +0900

    ASDFASDF

--- frontend/jest.base.mjs
+++ frontend/jest.base.mjs
@@ -2,7 +2,7 @@ export default {
     rootDir: '.',
     testEnvironment: 'node',
     roots: ['<rootDir>/src/__tests__'],
-    testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+    testMatch: ['**/*.test.ts', '**/*.test.tsx'], // 안전한 2패턴
     transform: {
         '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true, tsconfig: '<rootDir>/tsconfig.jest.json' }]
     },
